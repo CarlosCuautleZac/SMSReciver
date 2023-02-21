56b4d799a4cec9022e9c0d5de9867a7a75e55f28
alumnocca5 <alumnocca5@CCCA5-014>
2023-02-21 17:54:05 -0600

    WPF

---
+++ SMSRelayWPF/Models/SMS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMSRelayWPF.Models
+{
+    public class SMS
+    {
+        public string Mensaje { get; set; } = "";
+        public DateTime Fecha { get; set; }
+        public string Contacto { get; set; } = "";
+
+        public override string ToString()
+        {
+            return $"Contacto:{Contacto}\n {Mensaje}";
+        }
+    }
+}
