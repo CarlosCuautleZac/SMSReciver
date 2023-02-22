c87b75375bc7f73785f1537d13ece30e48bb93cd
CarlosCuautleZac <191g0235@rcarbonifera.tecnm.mx>
2023-02-22 12:01:10 -0600

    Añadido de metodos en wpf

---
+++ SMSRelayWPF/Helpers/ConvertSMS.cs
@@ -0,0 +1,70 @@
+﻿using SMSRelayWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SMSRelayWPF.Helpers
+{
+    public class ConvertSMS
+    {
+        public SMS ConvertirSMS(string inputString)
+        {
+            SMS sms = new SMS();
+
+            var x = ParseRow(inputString);
+
+            if (x.ContainsKey("body"))
+                sms.Mensaje = x["body"];
+
+            if (x.ContainsKey("address"))
+                sms.Contacto = x["address"];
+
+            if (x.ContainsKey("date"))
+            {
+                long unixDate = long.Parse(x["date"]);
+                DateTime start = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+                DateTime date = start.AddMilliseconds(unixDate).ToLocalTime();
+                sms.Fecha = date;
+            }
+            return sms;
+        }
+
+        Dictionary<string, string> ParseRow(string inputString)
+        {
+
+            Encoding iso = Encoding.GetEncoding("ISO-8859-1");
+
+            // Decodificar la cadena y convertirla a UTF-8
+            byte[] isoBytes = iso.GetBytes(inputString);
+            string utf8String = Encoding.UTF8.GetString(isoBytes);
+
+
+            Dictionary<string, string> rowDict = new Dictionary<string, string>();
+
+            // Definir la expresión regular para extraer las llaves y valores
+            //string pattern = @"(\w+)=(\S+),?";
+            string pattern = @"(\w+)=([\s\w:.\/\+\?¿:#áéíóúÁÉÍÓÚñÑ]+),?";
+
+            Regex regex = new Regex(pattern);
+
+            // Obtener todas las coincidencias de la expresión regular en la entrada
+            MatchCollection matches = regex.Matches(utf8String);
+
+            // Iterar sobre las coincidencias y agregarlas al diccionario
+            foreach (Match match in matches)
+            {
+                string key = match.Groups[1].Value;
+                string value = match.Groups[2].Value;
+                rowDict[key] = value;
+            }
+
+            return rowDict;
+        }
+    }
+
+
+    
+}
--- SMSRelayWPF/ViewModels/MainViewModel.cs
+++ SMSRelayWPF/ViewModels/MainViewModel.cs
@@ -1,12 +1,168 @@
-﻿using System;
+﻿using SharpAdbClient;
+using SMSRelayWPF.Helpers;
+using SMSRelayWPF.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace SMSRelayWPF.ViewModels
 {
-    class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+
+
+        public List<SMS>? Actuales { get; set; } = new();
+        public List<SMS>? Nuevos { get; set; } = new();
+
+        public List<SMS>? SMSPantalla { get; set; } = new();
+
+        string command { get; set; }
+        bool conectado = false;
+        long unixtime;
+
+        #region Objects
+        AdbClient adb;
+        //SerialPort puertoSerie;
+        ConvertSMS convertSMS;
+        DeviceData? device;
+        Dispatcher dispatcher;
+
+
+
+        #endregion
+        public MainViewModel()
+        {
+            adb = new AdbClient();
+            //puertoSerie = new SerialPort("COM3", 9600);
+            convertSMS = new();
+            dispatcher = Dispatcher.CurrentDispatcher;
+            Iniciar();
+        }
+
+        private void Iniciar()
+        {
+            bool result = false;
+
+            Task task = new Task(() =>
+            {
+                result = Conect();
+                Actualizar();
+            });
+
+            task.Start();
+            task.Wait();
+
+            if (result)
+            {
+                Thread t = new Thread(() =>
+                {
+                    while (true)
+                    {
+                        try
+                        {
+
+                            var reciver = new ConsoleOutputReceiver();
+                            adb.ExecuteRemoteCommand(command, device, reciver);
+                            var messages = reciver.ToString().Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None); ;
+                            Nuevos = messages.Select(x => convertSMS.ConvertirSMS(x)).ToList();
+
+
+
+                            var recienllegados = Nuevos.Where(x => !Actuales.Any(y => y.Mensaje == x.Mensaje && y.Contacto == x.Contacto && y.Fecha == x.Fecha)).ToList();
+                            //if(recienllegados.Count > 300) 
+                            //{
+                            //    recienllegados.Clear();
+                            //}
+
+                            //recienllegados.ForEach(x => Console.WriteLine(x));
+                            foreach (var item in recienllegados)
+                            {
+                                dispatcher.Invoke(() =>
+                                {
+                                    if (SMSPantalla != null)
+                                        SMSPantalla.Add(item);
+                                    Actualizar();
+                                });
+
+                                //puertoSerie.Write(item.Contacto + "," + item.Mensaje);
+
+
+                            }
+
+                            dispatcher.Invoke((Action)(() =>
+                            {
+                                if (Actuales != null)
+                                    Actuales.AddRange(recienllegados);
+
+                                Actualizar();
+                            }));
+
+                            
+
+
+
+                            Thread.Sleep(2000);
+                        }
+                        catch (Exception m)
+                        {
+                            Console.WriteLine(m.Message);
+                        }
+                    }
+                });
+
+                t.IsBackground = true;
+                t.Start();
+            }
+        }
+
+        private bool Conect()
+        {
+            try
+            {
+                //puertoSerie.Open();
+                conectado = true;
+                if (conectado)
+                {
+                    DateTime currentTime = DateTime.Now;
+                    long unixTime = ((DateTimeOffset)currentTime).ToUnixTimeSeconds();
+                    command = $"content query --uri content://sms/inbox --projection address,date,body --where \"date>={unixTime}\"";
+                    device = adb.GetDevices().FirstOrDefault();
+
+                    if (device == null)
+                    {
+                        throw new ApplicationException("No Android device found.");
+
+                    }
+                    var reciver = new ConsoleOutputReceiver();
+                    adb.ExecuteRemoteCommand(command, device, reciver);
+
+                    var messages = reciver.ToString().Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None); ;
+
+                    List<SMS> Actuales = messages.Select(x => convertSMS.ConvertirSMS(x)).OrderByDescending(x => x.Fecha).ToList();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                conectado = false;
+                //puertoSerie.Close();
+                return false;
+            }
+        }
+
+        public void Actualizar(string nombre = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
